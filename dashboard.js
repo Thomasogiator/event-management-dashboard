e0d81670d669555dc705ea2287a57e0193937ab2
Thomas Ogiator <thomasogiator@gmail.com>
2024-10-22 11:17:15 +0100

    updates

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,4 @@
-$('.carousel').carousel()
+// $('.carousel').carousel()
 
 function updateImage() {
     const img = document.getElementById('responsiveImage');
@@ -16,3 +16,16 @@ updateImage();
 
 // Update image on window resize
 window.addEventListener('resize', updateImage);
+
+const handleMenu = () =>{
+    let menu_btn = document.querySelector('.hamburger')
+    let menu_image = document.querySelector('hamburger-img')
+
+    menu_btn.onclick = function(){
+        menu_btn.classList.toggle('isactive')
+        menu_image.src === './assets/close.svg'
+    }
+    // alert('clicked')
+    // console.log('this is it');
+    
+}
